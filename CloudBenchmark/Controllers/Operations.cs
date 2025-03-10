5c361e65904d86614e20a7fa0773c901aa1d1f3b
manar.alkul <manar.alkul@connamix.com>
2025-03-10 14:16:20 +0300

    file read/write operations

---
+++ CloudBenchmark/Controllers/Operations.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace CloudBenchmark.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Operations : ControllerBase
+    {
+        [HttpGet]
+        [Route("fileWrite")]
+        //https://localhost:7167/api/Operations/fileWrite?fileSizeMB=100
+        public double fileWrite([FromQuery] int fileSizeMB)
+        {
+            string filePath = $"fileWrite-{DateTime.UtcNow.Ticks}.bin";
+            int fileSize = fileSizeMB * 1024 * 1024; // to MB
+            byte[] data = new byte[fileSize];
+            new Random().NextBytes(data);
+
+            // Measure write speed
+            Stopwatch sw = Stopwatch.StartNew();
+            System.IO.File.WriteAllBytes(filePath, data);
+            sw.Stop();
+
+            System.IO.File.Delete(filePath);
+            return Math.Round(sw.Elapsed.TotalMilliseconds);
+        }
+        
+        [HttpGet]
+        [Route("fileRead")]
+        //https://localhost:7167/api/Operations/fileRead?fileSizeMB=100
+        public double fileRead([FromQuery] int fileSizeMB)
+        {
+            string filePath = $"fileRead-{DateTime.UtcNow.Ticks}.bin";
+            int fileSize = fileSizeMB * 1024 * 1024; // to MB
+            byte[] data = new byte[fileSize];
+            new Random().NextBytes(data);
+            System.IO.File.WriteAllBytes(filePath, data);
+
+            // Measure write speed
+            Stopwatch sw = Stopwatch.StartNew();
+            byte[] readData = System.IO.File.ReadAllBytes(filePath);
+            sw.Stop();
+
+            System.IO.File.Delete(filePath);
+            return Math.Round(sw.Elapsed.TotalMilliseconds);
+        }
+    }
+}
